935e157e6402cab3456442a5ec1bacb8b6f4eaa5
Conner Charlebois <cdcharlebois@gmail.com>
2017-10-27 17:53:08 -0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require("gulp");
+
+/**
+ * Move the CSS files from CSS to /deployment/web/styles/css/{custom|lib}
+ */
+gulp.task("copy-css", function() {
+    console.log("moving CSS files to deployment dir");
+    gulp.src(["theme/styles/css/**/*.css"])
+        .pipe(gulp.dest("deployment/web/styles/css"));
+});
+
+gulp.task("watch", function() {
+    gulp.watch(["theme/styles/css/**/*.css"], ["copy-css"]);
+});
+
+gulp.task("default", ["watch"]);
